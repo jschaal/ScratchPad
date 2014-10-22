750681528be984d2832b60e5b1a6713a920e51a2
Joe Schaal <joe@schaal.net>
2014-10-22 14:57:11 -0400

    Initial commit

---
+++ functions.R
@@ -0,0 +1,17 @@
+make.power <- function(n) {
+        pow <- function(x) {
+                x^n
+        }
+        pow
+}
+
+y <- 10
+
+f <- function(x) {
+        y <-2
+        y^2 + g(x)
+}
+
+g <- function(x) {
+        x*y
+}
